660658e46b6eff61e5809c703f7483338015f6bb
yalexwander <91904134+yalexwander@users.noreply.github.com>
2021-10-10 01:54:12 +0300

    Added cache/storage to keep values between fetches.

--- lib/ItIsAllMail/Utils/Storage.php
+++ lib/ItIsAllMail/Utils/Storage.php
@@ -4,13 +4,48 @@ namespace ItIsAllMail\Utils;
 
 class Storage
 {
-    public function get(string $key): string
+    protected static $storageDir =  __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".."
+        . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "cache" . DIRECTORY_SEPARATOR;
+
+    public static function get(string $driver, string $key): ?string
     {
-        return "";
+        $driverDir = self::$storageDir . self::sanitizeFilename($driver);
+        if (! file_exists($driverDir)) {
+            return null;
+        }
+
+        $keyFilename = $driverDir . DIRECTORY_SEPARATOR . self::sanitizeFilename($key);
+        if (! file_exists($keyFilename)) {
+            return null;
+        }
+
+        return file_get_contents($keyFilename);
     }
 
-    public function set(string $key, string $value): void
+    /**
+     * Saves value to a file in the driver's cache directory
+     */
+    public static function set(string $driver, string $key, string $value): void
     {
-        return;
+        $driverDir = self::$storageDir . self::sanitizeFilename($driver);
+        if (! file_exists($driverDir)) {
+            mkdir($driverDir);
+        }
+
+        $keyFilename = $driverDir . DIRECTORY_SEPARATOR . self::sanitizeFilename($key);
+        file_put_contents($keyFilename, $value);
+    }
+
+    /**
+     * Try to keep balance betwee readability and security for possible theing
+     * like nicknames, urls, emails, etc
+     */
+    protected static function sanitizeFilename(string $key)
+    {
+        return preg_replace(
+            '/[^A-Za-z0-9@\._]+/',
+            '-',
+            $key
+        );
     }
 }
