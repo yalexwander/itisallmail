cb2ace4693b041de28c5d735ffb75df072d6b59f
yalexwander <91904134+yalexwander@users.noreply.github.com>
2021-10-16 21:40:18 +0300

    Added fail handling for drivers.

--- scripts/fetcher.php
+++ scripts/fetcher.php
@@ -18,11 +18,19 @@ $driverFactory = new FetchDriverFactory($config);
 foreach ($sources as $source) {
     $driver = $driverFactory->getFetchDriverForSource($source);
 
-    $posts = $driver->getPosts($source);
-
     $mailboxPath = $config["mailbox_base_dir"] . DIRECTORY_SEPARATOR .
         (($source["mailbox"] ?? $driver->getOpt("mailbox")) ?? $config["mailbox"]);
-
     $m = new Mailbox($mailboxPath);
-    $m->mergeMessages($posts);
+
+
+    // We have 2 main fail points here:
+    // 1) problems with site like connection or markup changes
+    // 2) Producing emails incompatible with standards
+    try {
+        $posts = $driver->getPosts($source);
+        $m->mergeMessages($posts);
+    } catch (\Exception $e) {
+        printf("Failed to process source %s with driver %s\n", $source["url"], $driver->getCode());
+        printf("Details:\n%s\n", $e->__toString());
+    }
 }
