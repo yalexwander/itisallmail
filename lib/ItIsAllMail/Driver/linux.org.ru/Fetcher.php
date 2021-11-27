7a4cd58dd5ef452b7dcd4348c8a1dd62ba881dfd
yalexwander <91904134+yalexwander@users.noreply.github.com>
2021-11-27 15:47:29 +0300

    Added another HTML parser library. Moved linux.org.ru driver to it.

--- lib/ItIsAllMail/Driver/linux.org.ru/Fetcher.php
+++ lib/ItIsAllMail/Driver/linux.org.ru/Fetcher.php
@@ -11,10 +11,10 @@ use ItIsAllMail\Utils\Browser;
 use ItIsAllMail\Utils\Debug;
 use ItIsAllMail\Utils\MailHeaderProcessor;
 use ItIsAllMail\Utils\URLProcessor;
-use PHPHtmlParser\Dom;
-use PHPHtmlParser\Dom\HtmlNode;
+use voku\helper\HtmlDomParser;
+use voku\helper\SimpleHtmlDom;
 
-class LORDriver extends AbstractFetcherDriver implements FetchDriverInterface
+class LinuxOrgRuFetcher extends AbstractFetcherDriver implements FetchDriverInterface
 {
     protected $crawler;
     protected $driverCode = "linux.org.ru";
@@ -38,17 +38,15 @@ class LORDriver extends AbstractFetcherDriver implements FetchDriverInterface
 
         while ($url) {
             $html = Browser::getAsString($url);
-            //dirtiest fix for PHPHtmlParser
-            $html = preg_replace('/<meta\s+property=\s*\"[^\"]+\"\s+content=\s*"[^\"]+"\s*\/*>/', "", $html);
 
-            $dom = new Dom();
-            $dom->loadStr($html);
+            $dom = HtmlDomParser::str_get_html($html);
 
-            foreach ($dom->find("article.msg") as $postNode) {
-                $isStartPost = $postNode->find(".msg_body > footer > div.sign > a")[0] !== null;
+            foreach ($dom->findMulti("article.msg") as $postNode) {
+                $isStartPost = $postNode->findOneOrFalse(".msg_body > footer > div.sign > a");
 
-                $author = $postNode->find(".msg_body div.sign > a")[0];
-                if ($author !== null) {
+                $author = $postNode->findOneOrFalse(".msg_body div.sign > a");
+
+                if ($author !== false) {
                     $author = $author->text();
                 } else {
                     continue;
@@ -81,10 +79,11 @@ class LORDriver extends AbstractFetcherDriver implements FetchDriverInterface
                 $posts[] = $msg;
             }
 
-            $nextPage = $dom->find("#comments .nav a.page-number");
-            $nextPage = $nextPage[count($nextPage) - 1];
-            if (($nextPage !== null) and strstr($nextPage->text, "→")) {
-                $url = rtrim($dom->find("base")->getAttribute("href"), "/") . $nextPage->getAttribute("href");
+            $nextPage = $dom->findMulti("#comments .nav a.page-number");
+            $nextPage = $nextPage->offsetGet($nextPage->count() - 1);
+
+            if (($nextPage !== null) and strstr($nextPage->text(), "→")) {
+                $url = rtrim($dom->findOne("base")->getAttribute("href"), "/") . $nextPage->getAttribute("href");
                 Debug::debug("New url: $url");
             } else {
                 $this->setLastURLVisited($threadId, $url);
@@ -99,17 +98,16 @@ class LORDriver extends AbstractFetcherDriver implements FetchDriverInterface
     /**
      * Convert to text readable by CLI mail client
      */
-    protected function getPostText(HtmlNode $post): string
+    protected function getPostText(SimpleHtmlDom $post): string
     {
-        $bodyWidget = $post->find(".msg_body")[0];
+        $bodyWidget = $post->findOne(".msg_body");
 
         $cleanedHTML = "";
 
         // bulky, but allows to parse op-post and comments in a same way
-        foreach ($bodyWidget->getChildren() as $tag) {
-            $tagName = $tag->getTag()->name();
+        foreach ($bodyWidget->childNodes() as $tag) {
+            $tagName = $tag->getNode()->nodeName;
             $class = $tag->getAttribute("class");
-
             if (
                 ($tagName === "div" and
                  (
@@ -122,7 +120,7 @@ class LORDriver extends AbstractFetcherDriver implements FetchDriverInterface
             ) {
                 continue;
             } else {
-                $cleanedHTML .= $tag->innerHtml;
+                $cleanedHTML .= $tag->outerHtml();
             }
         }
 
@@ -136,13 +134,13 @@ class LORDriver extends AbstractFetcherDriver implements FetchDriverInterface
     /**
      * Generate something can be put into "Subject" field
      */
-    protected function getPostTitle(HtmlNode $post, string $bodyText): string
+    protected function getPostTitle(SimpleHtmlDom $post, string $bodyText): string
     {
-        $titleWidget = $post->find("h1 > a")[0];
+        $titleWidget = $post->findOneOrFalse("h1 > a");
 
         $title = "";
-        if ($titleWidget !== null) {
-            $title = $titleWidget->text;
+        if ($titleWidget !== false) {
+            $title = $titleWidget->text();
         } else {
             $lines = explode("\n", $bodyText);
             foreach ($lines as $line) {
@@ -163,16 +161,22 @@ class LORDriver extends AbstractFetcherDriver implements FetchDriverInterface
     protected function getThreadIdFromURL(string $url): string
     {
         $id = null;
-        preg_match("/\/forum\/([^\/]+)\/([0-9]+)/", $url, $id);
-        return $id[1] . "_" . $id[2];
+        preg_match("/\/(forum|news)\/([^\/]+)\/([0-9]+)/", $url, $id);
+        return $id[1] . "_" . $id[2] . "_" . $id[3];
     }
 
-    protected function getParent(HtmlNode $node, string $defaultParent): string
+    protected function getParent(SimpleHtmlDom $node, string $defaultParent): string
     {
         $parent = $node->find("a")[0];
         if ($parent !== null) {
-            if (preg_match('/forum\/([^\/]+)\/([0-9]+)\?cid=([0-9]+)/', $parent->getAttribute("href"), $matches)) {
-                return $matches[1] . "_" . $matches[2] . "#" . $matches[3];
+            if (
+                preg_match(
+                    '/(forum|news)\/([^\/]+)\/([0-9]+)\?cid=([0-9]+)/',
+                    $parent->getAttribute("href"),
+                    $matches
+                )
+            ) {
+                return $matches[1] . "_" . $matches[2] . "_" . $matches[3] . "#" . $matches[4];
             }
         }
 
@@ -183,7 +187,7 @@ class LORDriver extends AbstractFetcherDriver implements FetchDriverInterface
      * Try to extract post date. We have at least 2 html formats here. For old
      * topics and new ones. Probably more, so fallback to current date.
      */
-    protected function getPostDate(HtmlNode $post): \DateTime
+    protected function getPostDate(SimpleHtmlDom $post): \DateTime
     {
         $dateWidget = $post->find("div.sign > time")[0];
 
@@ -195,7 +199,7 @@ class LORDriver extends AbstractFetcherDriver implements FetchDriverInterface
     }
 
 
-    protected function getPostId(HtmlNode $post): string
+    protected function getPostId(SimpleHtmlDom $post): string
     {
         preg_match('/\-([0-9]+)/', $post->getAttribute("id"), $id);
         return $id[1];
