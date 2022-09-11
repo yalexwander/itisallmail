fd5e581078a87f7293a1db70845ee23281bf4e72
yalexwander <yalexwander@users.noreply.github.com>
2022-09-11 05:13:37 +0300

    Fix for updating headers

--- lib/ItIsAllMail/MailboxUpdater.php
+++ lib/ItIsAllMail/MailboxUpdater.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * !!! Seems the only correct(but slow) way to update message headers is to:
+ * 1) fully parse old message and extract attachements from it
+ * 2) Create completely new message with changed headers
+ * 3) Attach all attachements from old message
+ * 4) Rewrite the old message file with serialized data
+ *
+ * Here is 2 function that works more or less:
+ *
+ *     updateMessageHeaders:
+ * fast but
+ * dirty way with preserving attachements and with dirty header update, can
+ * break if parser or serializer will be changed or new headers to update
+ * added
+ *
+ *     updateMessageHeadersRebuild:
+ * Loses attachements but will not broke headers in any circuimstances
+ */
+
 namespace ItIsAllMail;
 
 use ItIsAllMail\CoreTypes\SerializationMessage;
@@ -17,6 +36,13 @@ class MailboxUpdater
     protected $sourceConfig;
     protected $headersToUpdate;
 
+    // anywhere in the system headers tried to keep lowercased. But MIME
+    // serializators use capitalized names, at least for standard headers
+    protected $headerMap = [
+        "subject" => "Subject",
+        Constants::IAM_HEADER_STATUSLINE => Constants::IAM_HEADER_STATUSLINE
+    ];
+
     public function __construct(HierarchicConfigInterface $sourceConfig)
     {
         $this->sourceConfig = $sourceConfig;
@@ -30,7 +56,7 @@ class MailboxUpdater
         }
     }
 
-    public function updateMessageHeaders(string $sourceMIMEFile, SerializationMessage $msg): int
+    public function updateMessageHeadersRebuild(string $sourceMIMEFile, SerializationMessage $msg): int
     {
         if (! count($this->headersToUpdate)) {
             return 0;
@@ -63,4 +89,48 @@ class MailboxUpdater
 
         return 0;
     }
+
+    public function updateMessageHeaders(string $sourceMIMEFile, SerializationMessage $msg): int
+    {
+        if (! count($this->headersToUpdate)) {
+            return 0;
+        }
+
+        if (! file_exists($sourceMIMEFile)) {
+            Debug::log("Probably racing condition with your MUA on {$sourceMIMEFile}");
+            return 0;
+        }
+
+        $oldMsgRaw = file_get_contents($sourceMIMEFile);
+        $newMsgRaw = $oldMsgRaw;
+        $oldMsg = EmailParser::parseMessage($oldMsgRaw);
+
+        $needUpdate = false;
+        foreach ($this->headersToUpdate as $header) {
+            $newHeader = $msg->getTranslatedMIMEHeader($header, $this->sourceConfig);
+            if (
+                ! empty($oldMsg["headers"][$header]) and
+                $oldMsg["headers"][$header] !== $newHeader
+            ) {
+                // print_r($oldMsg);
+                // print_r($newHeader);
+                Debug::debug("Header mismatch for \"{$header}\":\n\"{$oldMsg["headers"][$header]}\"\nvs\n\"{$newHeader}\"");
+
+                $headerOffsetStart = mb_strpos($oldMsgRaw, "\r\nSubject: ") + 2;
+                $headerOffsetEnd = mb_strpos($oldMsgRaw, "\r\n", $headerOffsetStart + 4);
+
+                $newMsgRaw = mb_substr($newMsgRaw, 0, $headerOffsetStart) . $this->headerMap[$header] . ": " . $newHeader . mb_substr($newMsgRaw, $headerOffsetEnd);
+                                    
+                $needUpdate = true;
+            }
+        }
+
+        if ($needUpdate) {
+            file_put_contents($sourceMIMEFile, $newMsgRaw);
+            return 1;
+        }
+
+        return 0;
+    }
+
 }
